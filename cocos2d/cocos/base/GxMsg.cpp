e7d3cef638b4f752681b36b5c4912d08c27ac48f
farlongchen <farlongchen@hotmail.com>
2017-06-30 12:14:44 +0800

    first

---
+++ cocos2d/cocos/base/GxMsg.cpp
@@ -0,0 +1,88 @@
+﻿#include "GxMsg.h"
+
+GxMsgQueue* GxMsgQueue::_share = NULL;
+
+GxMsgQueue::GxMsgQueue()
+{
+	mtx_wq = new std::mutex();
+}
+
+GxMsgQueue* GxMsgQueue::shared()
+{
+	if (_share==NULL) _share = new GxMsgQueue();
+	return _share;
+}
+
+//#include "XLog.h"
+
+
+GxMsgQueue::~GxMsgQueue()
+{
+	clear();
+//	XLOG_DEBUG("释放消息队列");
+	delete mtx_wq;
+}
+
+void GxMsgQueue::PushBack(const void * buf, size_t size, void * who)
+{
+	if (size < 1 || buf==NULL) return;
+
+	std::lock_guard<std::mutex> lck(*mtx_wq);
+	GxMsg* m = new GxMsg(buf, size, who);
+	waitQue.push_back(m);
+//	workQue.push_back(m);
+}
+
+void GxMsgQueue::LoopWork(void(cbWork)(char *,size_t,void*, void *), void * arg)
+{
+//	mtx_wq.lock();
+	std::lock_guard<std::mutex> lck(*mtx_wq);
+	while (!waitQue.empty())
+	{
+		GxMsg* m = waitQue.front();
+	
+#if(1)
+		workQue.push_back(m);
+#else
+		workQue.push_back(m->Clone());
+		m->release();
+#endif
+		waitQue.pop_front();
+	}
+	
+	while (!workQue.empty())
+	{
+		GxMsg* m = workQue.front();
+		if (cbWork) (cbWork)(m->Data(),m->Length(),m->Who(),arg);
+		//m->release();
+		ReleaseMsg(m);
+		workQue.pop_front();
+	}
+
+//	mtx_wq.unlock();
+
+}
+
+void GxMsgQueue::clear()
+{
+	std::lock_guard<std::mutex> lck(*mtx_wq);
+	while (!waitQue.empty()) {
+		GxMsg* m = waitQue.front();
+		//m->release();
+		ReleaseMsg(m);
+		waitQue.pop_front();
+	}
+
+	while (!workQue.empty())
+	{
+		GxMsg* m = workQue.front();
+		//m->release();
+		ReleaseMsg(m);
+		workQue.pop_front();
+	}
+}
+
+void GxMsgQueue::ReleaseMsg(GxMsg* m)
+{
+	delete m;
+}
---
+++ cocos2d/cocos/base/GxMsg.h
@@ -0,0 +1,88 @@
+﻿
+
+#ifndef _GXMSG_H_
+#define _GXMSG_H_
+
+
+#include <deque>
+#include <mutex>
+#include <memory.h>
+
+//#include "autorelease.h"
+
+typedef unsigned int uint;
+
+class GxMsg /*: public CxRef*/
+{
+	char* dat;
+	uint   len;
+	void* owner;
+
+public:
+
+	GxMsg(const void* buf,uint size,void* _who)
+	{
+//		autorelease();
+		dat = NULL;
+		len = size;
+		owner = _who;
+		if (len) {
+			dat = new char[len + 1];
+			dat[len] = '\0';
+			memcpy(dat, buf, len);
+		}
+	}
+
+	virtual ~GxMsg()
+	{
+		if (dat) delete[] dat;
+//		fprintf(stderr, "--------释放内存=========++++++");
+	}
+
+	char* Data() { return dat; }
+	uint  Length() { return len; }
+	void* Who() { return owner; }
+
+	GxMsg* Clone()
+	{
+		return new GxMsg(dat, len, owner);
+	}
+
+};
+
+
+class GxMsgQueue
+{
+private:
+	static GxMsgQueue* _share;
+
+protected:
+	std::deque<GxMsg*> workQue;
+	std::deque<GxMsg*> waitQue;
+	std::mutex* mtx_wq;
+
+public:
+	GxMsgQueue();
+
+	static GxMsgQueue* shared();
+
+	virtual ~GxMsgQueue();
+
+	void PushBack(const void* buf, size_t size, void* who);
+
+
+	void LoopWork(void(cbWork)(char *, size_t, void*, void *), void * arg);
+	void clear();
+
+	void ReleaseMsg(GxMsg* m);
+
+};
+
+
+
+#endif // !_GXMSG_H_
+
+
+
+
+
