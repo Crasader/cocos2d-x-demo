404b69db212b2e50cf41172f2f7ee36840f02749
farlongchen <farlongchen@hotmail.com>
2017-07-08 18:33:42 +0800

    程序结构设计

--- Game/XBag.cpp
+++ Game/XBag.cpp
@@ -6,3 +6,19 @@ std::unordered_map<XBC_INT, XBC_INT> theItemRulerCombine;
 std::unordered_map<XBC_INT, XBC_INT> theItemRulerSort;
 
 
+void GxBag::SetDataSource(GxBagDataSource* _source)
+{
+	m_pSource = _source;
+}
+
+void GxBag::SetDataStorage(GxBagDataStorage* _storage)
+{
+	m_pStorage = _storage;
+}
+
+void GxBag::Reload()
+{
+	if (m_pSource) {
+		m_pSource->Reload(this);
+	}
+}
--- Game/XBagIO.h
+++ Game/XBagIO.h
@@ -4,11 +4,18 @@
 #include "XBag.h"
 
 
+/**
+从网络中取得
+*/
 class GxBagDataSourceINet : public GxBagDataSource
 {
 public:
+	virtual void Reload(GxBag* target);
 
-
+	/**
+	收到背包信息反馈后处理
+	*/
+	void OnReloadResponse(const char* buf,size_t sz);
 
 };
 
